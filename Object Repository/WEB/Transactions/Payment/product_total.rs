c7475e6e67f2cbd99ce7cf2b565566d577a69885
salsabilarhdsy <salsabilarhdsy@gmail.com>
2022-05-14 13:52:56 +0700

    Test Suite Web - 14/05/2022

--- Object Repository/WEB/Transactions/Payment/product_total.rs
+++ Object Repository/WEB/Transactions/Payment/product_total.rs
@@ -6,14 +6,21 @@
    <elementGuidId>735c6e7d-28f7-4b9b-bcf8-666d4942dd0f</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'label-total-bayar']</value>
+      </entry>
+      <entry>
          <key>XPATH</key>
          <value>/html/body/div/div/main/div/div/div/div/div[1]/table/tbody/tr[1]/td[4]</value>
       </entry>
-      <entry>
-         <key>BASIC</key>
-         <value></value>
-      </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>label-total-bayar</value>
+   </webElementProperties>
 </WebElementEntity>
---
+++ Test Suites/MOBILE/MOBILE.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MOBILE</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>55939a8e-703a-4e14-8cd1-d418306c5b32</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca646b18-19c2-47f6-b28e-9621f65aa5e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MOBILE/Auth/Register</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3247a892-0315-431b-9e81-9c0212c7bf8f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MOBILE/Auth/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3bd61dc6-808d-4139-a0f3-2c2d271a52d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MOBILE/Auth/Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
